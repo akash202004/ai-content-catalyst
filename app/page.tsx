ca8dd3cc250b83ea59b9785b7a7de3b1cf3f55eb
akash202004 <akashlaha48@gmail.com>
2024-07-27 11:19:31 +0530

    project structure & auth using clerk

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Akash Laha</h1>
+      <Button>Click me</Button>
+    </div>
+  );
+}
---
+++ middleware.ts
@@ -0,0 +1,14 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) auth().protect();
+});
+
+const isProtectedRoute = createRouteMatcher(["/dashboard(.*)", "/"]);
+
+export const config = {
+  matcher: [
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    "/(api|trpc)(.*)",
+  ],
+};

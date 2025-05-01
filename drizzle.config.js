cadd4f2f3127e392d64c7a1e93a73d705cd25e2c
akash202004 <akashlaha48@gmail.com>
2025-05-01 16:19:21 +0530

    Update drizzle configuration and package dependencies

--- drizzle.config.js
+++
@@ -1,8 +0,0 @@
-/** @type { import("drizzle-kit").Config } */
-export default {
-    schema: "./utils/schema.tsx",
-    dialect: 'postgresql',
-    dbCredentials: {
-        url: 'postgresql://accounts_owner:V1ykqSbv5IKm@ep-long-rain-a55ob73f.us-east-2.aws.neon.tech/Ai-Content-Generator?sslmode=require',
-    }
-};
--- utils/db.tsx
+++ utils/db.tsx
@@ -1,9 +1,12 @@
-import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
+import { config } from "dotenv";
 import * as schema from "./schema";
 
-if (!process.env.NEXT_PUBLIC_DRIZZLE_DB_URL) {
+config({ path: ".env.local" });
+
+if (!process.env.DATABASE_URL) {
   throw new Error("NEXT_PUBLIC_DRIZZLE_DB_URL is not set");
 }
-const sql = neon(process.env.NEXT_PUBLIC_DRIZZLE_DB_URL!);
+const sql = neon(process.env.DATABASE_UR!);
 export const db = drizzle(sql, { schema });

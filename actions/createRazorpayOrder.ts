1ef24e65358f36128f87fe3f5340e4d0bd7c695c
akash202004 <akashlaha48@gmail.com>
2025-05-02 02:43:45 +0530

    Update Razorpay API key in .env.example and createRazorpayOrder.ts

--- actions/createRazorpayOrder.ts
+++ actions/createRazorpayOrder.ts
@@ -4,7 +4,7 @@ import Razorpay from "razorpay";
 
 const razorpay = new Razorpay({
   key_id: process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID,
-  key_secret: process.env.NEXT_PUBLIC_RAZORPAY_KEY_SECRET,
+  key_secret: process.env.RAZORPAY_SECRET_KEY,
 });
 
 export async function createRazorpayOrder(amount: number) {

908af4dd8f032458fb2579dbacb7f13196773a4b
Shubham-Rajpoot2 <shubhamrajpoot1100@gmail.com>
2025-10-09 16:16:54 +0530

    Added a back button in login and signup page

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,8 +1,15 @@
 import { SignUp } from "@clerk/nextjs";
+import Link from "next/link";
 
 export default function Page() {
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className="flex items-center justify-center min-h-screen ">
+        <Link
+        href="/"
+        className="absolute top-4 left-4 bg-gray-900 hover:bg-gray-800 text-white font-medium px-4 py-1 rounded-md shadow-sm transition-all"
+      >
+        ⬅ Back
+      </Link>
       <SignUp />
     </div>
   );
